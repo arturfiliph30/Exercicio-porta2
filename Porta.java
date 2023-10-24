d0177cc30664a0292027d0c84b3459bc2189f994
FELIPE MOREIRA <143669527+arturfiliph30@users.noreply.github.com>
2023-10-23 23:03:41 -0400

    Add files via upload

---
+++ Porta.java
@@ -0,0 +1,18 @@
+package aulascanner.aula;
+
+public class Porta {
+	public static void main(String...args) {
+	
+		Integer chave=10;
+		
+		while(chave<15) {
+			
+			System.out.println
+			("Porta ainda esta fechada. Chave " + chave + " Não é a correta para abrir a porta!");
+			chave++;
+		}
+			
+				
+	}
+
+}
